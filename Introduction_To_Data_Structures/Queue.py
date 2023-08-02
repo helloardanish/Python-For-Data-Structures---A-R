bbeea0791e2ae7b46b8e11205d60983f535aac50
A R DANISH <helloardanish@gmail.com>
2023-08-03 02:03:03 +0300

    Add files via upload

---
+++ Introduction_To_Data_Structures/Queue.py
@@ -0,0 +1,44 @@
+from collections import deque
+#list
+#deque
+
+my_queu1 = []
+
+
+my_queu1.append(1)
+my_queu1.append(2)
+my_queu1.append(3)
+my_queu1.append(4)
+my_queu1.append(5)
+
+
+#print(my_queu1)
+
+my_queu1.pop(0)
+
+#print(my_queu1)
+
+my_queu1.pop(0)
+
+#print(my_queu1)
+
+my_queu2 = deque()
+
+
+my_queu2.append(10)
+my_queu2.append(11)
+my_queu2.append(12)
+my_queu2.append(13)
+my_queu2.append(14)
+
+print(my_queu2)
+
+my_queu2.popleft()
+
+print(my_queu2)
+
+my_queu2.popleft()
+
+print(my_queu2)
+
+
